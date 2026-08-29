130a748a7d52ff649c8276748c4bcc7bde64cf8a
Kousttav Biswas <kousttavb@gmail.com>
2026-08-29 17:02:52 +0530

    Exam

--- src/components/Award.jsx
+++ src/components/Award.jsx
@@ -3,6 +3,7 @@ import img1 from "../assets/massart.png";
 
 const Award = () => {
   const awards = [img1, img1, img1, img1, img1];
+
   const [current, setCurrent] = useState(0);
 
   const nextSlide = () => {
@@ -16,29 +17,39 @@ const Award = () => {
   };
 
   return (
-    <section className="award-slider-container">
-      <button className="arrow left" onClick={prevSlide}>
-        ←
-      </button>
-
-      <div className="slider-wrapper">
-        <div
-          className="slider-track"
-          style={{
-            transform: `translateX(-${current * 100}%)`,
-          }}
-        >
-          {awards.map((award, index) => (
-            <div className="slide" key={index}>
-              <img src={award} alt="Award" />
-            </div>
-          ))}
-        </div>
+    <section className="award">
+      <div className="award-heading">
+        <h1>Award</h1>
+        <p>
+          Learning provides the environment and inspiration to create
+          and exhibit that brings people together to build connections
+          to community.
+        </p>
       </div>
 
-      <button className="arrow right" onClick={nextSlide}>
-        →
-      </button>
+      {/* Desktop */}
+      <div className="award-container">
+        {awards.map((award, index) => (
+          <div className="award-item" key={index}>
+            <img src={award} alt="Award" />
+          </div>
+        ))}
+      </div>
+
+      {/* Mobile Slider */}
+      <div className="award-mobile-slider">
+        <button className="arrow left" onClick={prevSlide}>
+          ←
+        </button>
+
+        <div className="mobile-slide">
+          <img src={awards[current]} alt="Award" />
+        </div>
+
+        <button className="arrow right" onClick={nextSlide}>
+          →
+        </button>
+      </div>
     </section>
   );
 };
